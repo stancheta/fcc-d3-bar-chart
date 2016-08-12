7bd3a87f62f46ee16b05cc2cebb804aaa6ddb26e
Steven T Ancheta <steventancheta@gmail.com>
2016-08-12 09:15:45 -0700

    Formatted Data, added base d3

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,5 @@
+/* globals XMLHttpRequest */
+
 (function() {
   var dataURL = 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json';
 
@@ -20,15 +22,30 @@
       console.log('There was a connection error');
     };
     request.send();
-  }
+  };
 
-  handleData = function(data) {
+  var handleData = function(data) {
     var margin = {top: 10, right: 30, bottom: 30, left: 30},
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
-    
-  }
+    var parseDate = d3.time.format("%Y-%m-%d").parse;
+    var x = d3.time.scale().range([0, width]);
+    var y = d3.scale.linear().range([height, 0]);
+
+    var fData = data.data.map(function(d) {
+      return {date: parseDate(d[0]), value: Number(d[1])};
+    });
+    console.log(fData);
+    var svg = d3.select("body")
+		  .append("svg")
+		  .attr("width", width + margin.left + margin.right)
+		  .attr("height", height + margin.top + margin.bottom)
+		  .append("g")
+		  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+  };
 
   // setup
   getData(dataURL, handleData);
